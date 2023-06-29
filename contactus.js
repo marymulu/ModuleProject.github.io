33fab56ca26baedb52f06bbda13d554eb40f7bc6
marymulu <133064728+marymulu@users.noreply.github.com>
2023-06-29 12:04:22 -0400

    changes

--- contactus.js
+++ contactus.js
@@ -1,16 +1,5 @@
-function clicked(e) {
+function clicked(event) {
   if (!confirm("Thank you for your feedback.You'll hear back soon!")) {
-    e.preventDefault();
+    event.preventDefault();
   }
 }
-
-// document
-//   .getElementById("contactForm").addEventListener("submit", function clicked(event) {
-//     event.preventDefault(); // Prevent form submission
-
-//     // Display pop-up message
-//     alert("Thank you for your feedback. You'll hear back soon!");
-
-//     // Clear form fields
-//     this.reset();
-//   });
